9f1823e49e5b1205e1637aa6645456ccb9cb892c
Chenyang Li <cli040@ucr.edu>
2022-04-08 14:05:48 -0700

    Add healthcheck log

--- MongoFlask/get_request_status.py
+++ MongoFlask/get_request_status.py
@@ -1,18 +1,39 @@
 import requests
 import sys
+import os
+import time
+import datetime
 
 link_lists =['','/chart_allpowercontrols','/min_max_allpower_chart?var=1']
 
+health_log = os.environ['UPLOADPATH'] + os.environ['RACKNAME'] + '-health.log'
+
+start_time = time.time()
 for link in link_lists:
     try:
         cur_request = requests.get(sys.argv[1] + link)
         if cur_request.status_code != 200:
-            print('Error code: ' + str(cur_request.status_code))
+            print('Error code: ' + str(cur_request.status_code), flush=True)
+            cur_time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            output = "--- %s | Health check faild: took %s seconds ---\n" % (cur_time, time.time() - start_time)
+            with open(health_log, "a") as myfile:
+                myfile.write(output)
+                myfile.write('Error code: ' + str(cur_request.status_code) + '\n')
             sys.exit(1)
         else:
-            print(sys.argv[1] + link + 'Response OK')
+            print(sys.argv[1] + link + 'Response OK', flush=True)
     except Exception as e:
-        print('Error message: ' + str(e))
+        print('Error message: ' + str(e), flush=True)
+        cur_time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+        output = "--- %s | Health check faild: took %s seconds ---\n" % (cur_time, time.time() - start_time)
+        with open(health_log, "a") as myfile:
+            myfile.write(output)
+            myfile.write('Error message: ' + str(e) + '\n')
         sys.exit(1)
 # if all links work, then it is healthy
+cur_time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+output = "--- %s | Health check passed all requests: took %s seconds ---\n" % (cur_time, time.time() - start_time)
+print(output, flush=True)
+with open(health_log, "a") as myfile:
+    myfile.write(output)
 sys.exit(0)
